3be55048e20a662876fb462d1f9598194042601e
DEVpjy5658 <pjy5658@gmail.com>
2023-02-13 01:15:32 -0700

    Finished portfolio. Added testimonials

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com/DEVpjy5658',
+    demo: 'https://dribbble.com/search/crypto-currency-dashboard'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,71 +59,24 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG1} alt='' />
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio_item'>
+              <div className='portfolio_item-image'>
+              <img src={image} alt={title} />
           </div>
-          <h3>Item title</h3>
+          <h3>{title}</h3>
           <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-          </article>
-
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG2} alt='' />
-          </div>
-          <h3>Item title</h3>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
+            <a href={github} className='btn' target='_blank'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
+            )
+          })
+        }
 
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG3} alt='' />
-          </div>
-          <h3>Item title</h3>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG4} alt='' />
-          </div>
-          <h3>Item title</h3>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG5} alt='' />
-          </div>
-          <h3>Item title</h3>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG6} alt='' />
-          </div>
-          <h3>Item title</h3>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/Harvest-Builders/Samurai-Pizza-Cats-JinyoungPark' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonial</h2>
+
+      <div className='container testimonials_container'>
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src={AVTR1} alt='Avatar One' />
+          </div>
+          <h5 className='client_name'>Ernest Achiever</h5>
+          <small className='client_review'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Impedit velit exercitationem necessitatibus, alias consequuntur 
+              laboriosam quod, sed corrupti mollitia esse modi eos est 
+              blanditiis soluta a sint asperiores quisquam quam.
+          </small>
+        </article>
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src={AVTR1} alt='Avatar One' />
+          </div>
+          <h5 className='client_name'>Ernest Achiever</h5>
+          <small className='client_review'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Impedit velit exercitationem necessitatibus, alias consequuntur 
+              laboriosam quod, sed corrupti mollitia esse modi eos est 
+              blanditiis soluta a sint asperiores quisquam quam.
+          </small>
+        </article>
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src={AVTR1} alt='Avatar One' />
+          </div>
+          <h5 className='client_name'>Ernest Achiever</h5>
+          <small className='client_review'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Impedit velit exercitationem necessitatibus, alias consequuntur 
+              laboriosam quod, sed corrupti mollitia esse modi eos est 
+              blanditiis soluta a sint asperiores quisquam quam.
+          </small>
+        </article>
+      </div>
+    
+    </section>
   )
 }
 
